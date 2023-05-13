8c1ed84d208ae8c5bbcba6853d0486dd9cd28fa2
ssgll <guo.orm.o@gmail.com>
2023-05-13 17:40:18 +0800

    SpringMVC的简单配置模板

---
+++ src/main/java/com/ssgll/mvc/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.ssgll.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    public String hello(){
+        return "index";
+    }
+}
